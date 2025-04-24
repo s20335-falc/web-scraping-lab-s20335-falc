446271d9f0f277042558c98f759c1b009db59b75
isemscs <isurum@sci.pdn.ac.lk>
2025-04-25 04:17:10 +0530

    Added starter code

---
+++ WebScraper.java
@@ -0,0 +1,9 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+
+public class WebScraper {
+    public static void main(String[] args) {
+        // TODO: Scrape a URL and print its title
+    }
+}
