bf4aa5fc265b5712a2c0fd1bba39a42ceffd3d32
TotalCranberry <yuvindu2001@gmail.com>
2025-04-25 14:35:19 +0530

    Answer for the first subpart

--- WebScraper.java
+++ WebScraper.java
@@ -1,9 +1,39 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import java.io.IOException;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WebScraper {
     public static void main(String[] args) {
-        // TODO: Scrape a URL and print its title
+        String url = "https://bbc.com";  
+
+        try {
+            Document doc = Jsoup.connect(url).get();
+            
+            String title = doc.title();
+            System.out.println("Page Title: " + title);
+            System.out.println();
+
+            System.out.println("Headings:");
+            for (int i = 1; i <= 6; i++) {
+                Elements headings = doc.select("h" + i);
+                for (Element heading : headings) {
+                    System.out.println(heading.text());
+                }
+            }
+
+            System.out.println("\nLinks:");
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+                String linkHref = link.attr("abs:href");
+                String linkText = link.text();
+                System.out.println("  Text: " + linkText);
+                System.out.println("  URL: " + linkHref);
+                System.out.println();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error fetching the page: " + e.getMessage());
+        }
     }
 }
